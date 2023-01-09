7abe4a34992dcee2dcd93ac35a8428488368c4a2
code-cp <moshan.nus@gmail.com>
2023-01-09 19:50:35 +0800

    Create, edit and delete item by form

--- app.py
+++ app.py
@@ -17,18 +17,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-@app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
-@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
-def initdb(drop):
-    """Initialize the database."""
-    if drop:  # 判断是否输入了选项
-        db.drop_all()
-    db.create_all()
-    click.echo('Initialized database.')  # 输出提示信息
-
+# run flask forge to clear and create database 
 @app.cli.command()
 def forge():
     """Generate fake data."""
+    db.drop_all()
     db.create_all()
 
     # 全局的两个变量移动到这个函数内
@@ -36,14 +29,6 @@ def forge():
     movies = [
         {'title': 'My Neighbor Totoro', 'year': '1988'},
         {'title': 'Dead Poets Society', 'year': '1989'},
-        {'title': 'A Perfect World', 'year': '1993'},
-        {'title': 'Leon', 'year': '1994'},
-        {'title': 'Mahjong', 'year': '1996'},
-        {'title': 'Swallowtail Butterfly', 'year': '1996'},
-        {'title': 'King of Comedy', 'year': '1999'},
-        {'title': 'Devils on the Doorstep', 'year': '1999'},
-        {'title': 'WALL-E', 'year': '2008'},
-        {'title': 'The Pork of Music', 'year': '2012'},
     ]
 
     user = User(name=name)
