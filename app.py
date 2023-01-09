17348e2fc2188521aa34508bceda980441653293
code-cp <moshan.nus@gmail.com>
2023-01-09 15:56:49 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-from flask import Flask 
-from flask import url_for
+from flask import Flask, render_template, url_for
 from markupsafe import escape
 
 app = Flask(__name__)
 
 # can access from http://127.0.0.1:5000
-@app.route('/')
-def hello(): 
+# @app.route('/')
+# def hello(): 
     # return "欢迎来到我的观影清单！いらっしゃいませ！"
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
-    
+    # return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
 @app.route('/user/<name>')
 def user_page(name):
     return f'User: {escape(name)}'
@@ -26,4 +25,21 @@ def test_url_for():
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
 
+name = 'Tim'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
+    
