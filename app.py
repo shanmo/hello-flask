b2a115fab2616062e143b9aba5d09af723354f12
code-cp <moshan.nus@gmail.com>
2023-01-09 16:43:06 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,36 +1,29 @@
 from flask import Flask, render_template, url_for
 from markupsafe import escape
 from flask_sqlalchemy import SQLAlchemy
-import os
-import click
+import os, sys, click  
+
+# SQLite URI compatible
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
 db = SQLAlchemy(app)
 
-# can access from http://127.0.0.1:5000
-# @app.route('/')
-# def hello(): 
-    # return "欢迎来到我的观影清单！いらっしゃいませ！"
-    # return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
-
-@app.route('/user/<name>')
-def user_page(name):
-    return f'User: {escape(name)}'
-
-@app.route('/test')
-def test_url_for():
-    # 下面是一些调用示例（请访问 http://127.0.0.1:5000/test 后在命令行窗口查看输出的 URL）：
-    print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
-    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
-    print(url_for('user_page', name='harry'))  # 输出：/user/greyli
-    print(url_for('user_page', name='potter'))  # 输出：/user/peter
-    print(url_for('test_url_for'))  # 输出：/test
-    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
-    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
-    return 'Test page'
+@app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
 
 @app.cli.command()
 def forge():
@@ -61,13 +54,6 @@ def forge():
     db.session.commit()
     click.echo('Done.')
 
-@app.route('/')
-def index():
-    user = User.query.first()  # 读取用户记录
-    movies = Movie.query.all()  # 读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
-    
-
 class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
     id = db.Column(db.Integer, primary_key=True)  # 主键
     name = db.Column(db.String(20))  # 名字
@@ -77,11 +63,11 @@ class Movie(db.Model):  # 表名将会是 movie
     title = db.Column(db.String(60))  # 电影标题
     year = db.Column(db.String(4))  # 电影年份
 
-@app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
-@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
-def initdb(drop):
-    """Initialize the database."""
-    if drop:  # 判断是否输入了选项
-        db.drop_all()
-    db.create_all()
-    click.echo('Initialized database.')  # 输出提示信息
+@app.route('/')
+def index():
+    user = User.query.first()  # 读取用户记录
+    movies = Movie.query.all()  # 读取所有电影记录
+    return render_template('index.html', user=user, movies=movies)
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
